105fb92536056f9e9c365a7136fde7782d17345a
RogueSensei <3808255-roguesensei@users.noreply.gitlab.com>
2024-03-18 15:03:10 +0000

    More DAL

--- software-engineering-devops-qa/Controllers/EnrolmentController.cs
+++ software-engineering-devops-qa/Controllers/EnrolmentController.cs
@@ -1,15 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using software_engineering_devops_qa.Attributes;
 using software_engineering_devops_qa.Dal;
+using software_engineering_devops_qa.Models;
 
 namespace software_engineering_devops_qa.Controllers;
 
 public class EnrolmentController : BaseController
 {
 	[HttpGet("get")]
-	public IActionResult Get() => Ok(new EnrolmentDal().Get(Config.LmsDbConnection));
+	public IActionResult Get() => Ok(new EnrolmentDal()
+		.Get(Config.LmsDbConnection));
+
 
 	[AdminOnly]
 	[HttpPost("add")]
-	public IActionResult Add() => Ok();
+	public IActionResult Add([FromBody] Enrolment model) => Ok(new EnrolmentDal()
+		.Add(Config.LmsDbConnection, model));
+
+	[AdminOnly]
+	[HttpPost("update")]
+	public IActionResult Update([FromBody] Enrolment model) => Ok(new EnrolmentDal()
+		.Update(Config.LmsDbConnection, model));
+
+	[AdminOnly]
+	[HttpPost("delete")]
+	public IActionResult Delete([FromBody] int id) => Ok(new EnrolmentDal()
+		.Delete(Config.LmsDbConnection, id));
 }
